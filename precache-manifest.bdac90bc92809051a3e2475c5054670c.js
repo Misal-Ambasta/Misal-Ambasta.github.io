e0825ab37e1de4ddf10afd03caf751ee7ae81ba3
Misal-Ambasta <misalambasta@gmail.com>
2020-11-22 21:02:11 +0530

    Updates

---
+++ precache-manifest.bdac90bc92809051a3e2475c5054670c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49ae7449161cc311adcedea8df00e971",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bbec3fbceeded14b6627",
+    "url": "/static/css/main.cf915400.chunk.css"
+  },
+  {
+    "revision": "2254f519c123374b88c0",
+    "url": "/static/js/2.b413cdb6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b413cdb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbec3fbceeded14b6627",
+    "url": "/static/js/main.021dfc41.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
